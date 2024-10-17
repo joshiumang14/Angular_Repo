9c3a68c9156abeb84aba523ed73e5bdddc20fed5
Umang Joshi <124174591+joshiumang14@users.noreply.github.com>
2024-10-17 18:51:45 +0530

    Update app.module.ts
    dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import {ReactiveFormsModule } from '@angular/forms'; // Import FormsModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './features/header/header.component';
@@ -27,7 +27,7 @@ import { FooterComponent } from './features/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule // Add FormsModule here
+    ReactiveFormsModule // Add FormsModule here
   ],
   providers: [],
   bootstrap: [AppComponent]

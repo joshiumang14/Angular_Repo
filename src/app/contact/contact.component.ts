1166ef59e163d4f0e5656aaf42aea7aac94d8060
Umang Joshi <124174591+joshiumang14@users.noreply.github.com>
2024-10-17 18:46:52 +0530

    Update contact.component.ts
    gmail send done

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import emailjs from '@emailjs/browser';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-contact',
@@ -7,13 +10,34 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
-  
-  onSubmit(form: NgForm) {
-    if (form.valid) {
-      // Handle form submission logic here
-      console.log('Form Submitted!', form.value);
-      // Optionally, you can reset the form
-      form.reset();
-    }
+  form:FormGroup=this.fb.group({
+  from_name:'',
+  to_name:'Umang Joshi',
+  from_email:'',
+  Message:''
+  });
+  constructor(private fb: FormBuilder){
+
   }
+
+  async send(){
+    emailjs.init('K6qk74DBp4DyJvviA');
+    let response=await emailjs.send("service_3m2pkga","template_cnalpy2",{
+  from_name:this.form.value.from_name,
+  to_name: this.form.value.to_name,
+  from_email: this.form.value.from_email,
+  message: this.form.value.message,
+  });
+  alert('Message has been sent.');
+  this.form.reset();
+  }
+  
+   //onSubmit(form: NgForm) {
+  //   if (form.valid) {
+  //     // Handle form submission logic here
+  //     console.log('Form Submitted!', form.value);
+  //     // Optionally, you can reset the form
+  //     form.reset();
+  //   }
+  // }
 }
